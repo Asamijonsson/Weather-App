ec1407a182594c50c8f605f44b939bf59b5742a1
asamijonsson <momoji303@gmail.com>
2024-02-06 09:35:51 +0100

    added cvs file and check response status from api

--- weatherAnalysis.py
+++ weatherAnalysis.py
@@ -16,6 +16,34 @@ for city in cities:
     if not city:
         continue
 
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-response = requests.get(url)
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        data = response.json()
+        temp_kelvin = data['main']['temp']
+        temp_celsius = temp_kelvin - 273.15
+        desc = data['weather'][0]['description']
+
+        weatherData = {
+            'City': city,
+         'Temperature': temp_celsius,
+            'Description': desc
+        }
+        weatherList.append(weatherData)
+
+    else:
+        print('Error fetching weather data')
+
+df = pd.DataFrame(weatherList)
+
+# Save DataFrame to a CSV file
+df.to_csv('weatherData.csv', index=False)
+
+df.plot(x='City', y='Temperature', kind='bar', legend=False)
+plt.ylabel('Temperature (°C)')
+plt.title('Temperature in Cities')
+plt.show()
+
+# Display the DataFrame
+print(df)
