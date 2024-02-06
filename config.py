d2a330e951d45da336da2801dcc9c6c93a50af42
asamijonsson <momoji303@gmail.com>
2024-02-06 08:50:27 +0100

    added config

---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "56ae12255c16f1343f4b7dcf3d2a9871"
--- weatherAnalysis.py
+++ weatherAnalysis.py
@@ -1,8 +1,9 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import config
 
-api_key = "56ae12255c16f1343f4b7dcf3d2a9871"
+api_key = config.api_key
 
 city_input = input('Enter city names (comma-separated): ')
 
