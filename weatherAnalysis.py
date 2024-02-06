7d92550caa3bc44c9c8489b1023b6154a6126648
asamijonsson <momoji303@gmail.com>
2024-02-06 08:35:29 +0100

    added city input

--- weatherAnalysis.py
+++ weatherAnalysis.py
@@ -4,5 +4,17 @@ import matplotlib.pyplot as plt
 
 api_key = "56ae12255c16f1343f4b7dcf3d2a9871"
 
+city_input = input('Enter city names (comma-separated): ')
+
+cities = [city.strip() for city in city_input.split(',')]
+
+weatherList = []
+
+for city in cities:
+
+    if not city:
+        continue
+
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 response = requests.get(url)
+
