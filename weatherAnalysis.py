78fb07c568fe5b53a154ecf6b50b76b22e31b88c
asamijonsson <momoji303@gmail.com>
2024-02-09 09:32:01 +0100

    added while loop and fixed broken app

--- weatherAnalysis.py
+++ weatherAnalysis.py
@@ -8,61 +8,73 @@ import config
 # Hämta apikey från config
 apiKey = config.api_key
 
-# Be användaren att skriva in stadsnamn
-cityInput = input('Ange stadsnamn och separerade dem med komma: ')
+#Programmet kommer att fortsätta att köra så länge 'True' är sant. (När det t.ex kommer ett break eller exit, avslutas programmet)
+while True:
 
-# Skapa en lista, 'city' representerar varje stad i listan och strip() methoden ta bort extra mellanslag runt varje stad.
-# split(',') dela upp den i en lista av delsträngar vid varje kommatecken.
-cities = [city.strip() for city in cityInput.split(',')]
+    # Be användaren att skriva in stadsnamn
+    cityInput = input('Ange stadsnamn och separerade dem med komma: ')
 
-# weatherList kommer att användas för att lagra väderinformation 'weatherData'
-weatherList = []
+    # Skapa en lista, 'city' representerar varje stad i listan och strip() methoden ta bort extra mellanslag runt varje stad.
+    # split(',') dela upp den i en lista av delsträngar vid varje kommatecken.
+    cities = [city.strip() for city in cityInput.split(',')]
 
-# Loop som iterera över varje stad 'cities'
-for city in cities:
-# Om 'city' är tom, avsluta programmet
-    if not city:
-        print("Ingen stadsnamn. Avsluta programmet.")
-        exit()
+    # weatherList kommer att användas för att lagra väderinformation 'weatherData'
+    weatherList = []
 
-# Skapa URL, hämta info med stadsnamnet och api-nyckeln
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}'
-# Gör en HTTP GET request till url
-    response = requests.get(url)
+    # Loop som iterera över varje stad 'cities'
+    for city in cities:
+    # Om 'city' är tom, avsluta programmet
+        if not city:
+            print("Ingen stadsnamn. Avsluta programmet.")
+            exit()
 
-# Om vi får statuskod 200 OK
-    if response.status_code == 200:
-# Konvertarar JSON svaret och omvandlar om temp från kelvin till celsius.
-        data = response.json()
-        temp_kelvin = data['main']['temp']
-        temp_celsius = temp_kelvin - 273.15
-        desc = data['weather'][0]['description']
+    # Skapa URL, hämta info med stadsnamnet och api-nyckeln
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}'
+    # Gör en HTTP GET request till url
+        response = requests.get(url)
 
-# Skapar ett dictionary 'weatherData' och lägger till den i 'weatherList'
-        weatherData = {
-            'City': city,
-         'Temperature': temp_celsius,
-            'Description': desc
-        }
-        weatherList.append(weatherData)
+    # Om vi får statuskod 200 OK
+        if response.status_code == 200:
+    # Konvertarar JSON svaret och omvandlar om temp från kelvin till celsius.
+            data = response.json()
+            temp_kelvin = data['main']['temp']
+            temp_celsius = temp_kelvin - 273.15
+            desc = data['weather'][0]['description']
 
-# Om vi inte får statuskod 200 OK, skriv ett felmeddelande
+    # Skapar ett dictionary 'weatherData' och lägger till den i 'weatherList'
+            weatherData = {
+                'City': city,
+             'Temperature': temp_celsius,
+                'Description': desc
+            }
+            weatherList.append(weatherData)
+
+    # Om vi inte får statuskod 200 OK, skriv ett felmeddelande
+        else:
+            print('Error fetching weather data')
+
+    # Skapa en Pandas DataFrame från 'weatherList'
+    df = pd.DataFrame(weatherList)
+
+    # Spara DataFrame som en csv-fil
+    df.to_csv('weatherData.csv')
+
+   #Kontrollera om kolumnen 'City' finns i weatherList
+    if 'City' not in df.columns:
+        print("Skriv rätt stadsnamn.")
+    # Skapar ett stolpdiagram. X axeln = stadsnamn, Y axeln = temperature med diagrammet typen bar
     else:
-        print('Error fetching weather data')
+        df.plot(x='City', y='Temperature', kind='bar')
+        plt.ylabel('Temperature (°C)')
+        plt.title('Temperature in Cities')
 
-# Skapa en Pandas DataFrame från 'weatherList'
-df = pd.DataFrame(weatherList)
+    # Visa diagrammet
+    plt.show()
 
-# Spara DataFrame som en csv-fil
-df.to_csv('weatherData.csv')
+    # Skriv ut hela DataFrame till konsolen
+    print(df)
 
-# Skapar ett stolpdiagram. X axeln = stadsnamn, Y axeln = temperature med diagrammet typen bar
-df.plot(x='City', y='Temperature', kind='bar')
-plt.ylabel('Temperature (°C)')
-plt.title('Temperature in Cities')
-
-# Visa diagrammet
-plt.show()
-
-# Skriv ut hela DataFrame till konsolen
-print(df)
+#Om användaren skriver in fel stadsnamn, fråga om de vill köra programme igen eller avsluta.
+    runAgain = input("Vill du köra programmet igen? (Ja / Nej): ").lower()
+    if runAgain != 'ja':
+        break
